7b91cfbb51ae14e01350febb2d7f2485412bac57
Zachary Valentini <zvalenti@c3r3s2.42lausanne.ch>
2025-12-10 14:51:14 +0100

    hihiih

--- Cub3d/src/parsing/parse_color.c
+++ Cub3d/src/parsing/parse_color.c
@@ -6,7 +6,7 @@
 /*   By: zvalenti <zvalenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:47:36 by zvalenti          #+#    #+#             */
-/*   Updated: 2025/12/08 14:42:25 by zvalenti         ###   ########.fr       */
+/*   Updated: 2025/12/10 14:48:54 by zvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,39 +19,62 @@ static int convert_rgb_to_int(int r, int g, int b)
 
 static int is_valid_rgb(int r, int g, int b)
 {
-    if (r < 0 || r > 255)
-        return 0;
-    if (g < 0 || g > 255)
-        return (0);
-    if (b < 0 || b > 255)
-        return (0);
-    return (1);
+    return !(r < 0 || r > 255 ||
+             g < 0 || g > 255 ||
+             b < 0 || b > 255);
 }
 
 int parse_color(char *line, t_rgb *color)
 {
     char **split;
-    int     r;
-    int     g;
-    int     b;
+    char *r_str;
+    char *g_str;
+    char *b_str;
+    int   r, g, b;
 
     split = ft_split(line, ',');
-    if(!split)
-        return (1);
-    if (!split[0] || !split[1] || !split[2] || !split[3])
+    if (!split)
+        return (printf("Error\nMalloc failed\n"), 1);
+
+    // ❌ split[3] ne doit PAS exister, sinon il y a trop de valeurs
+    if (!split[0] || !split[1] || !split[2] || split[3] != NULL)
     {
         ft_free_tab(split);
-        return (1);
+        return (printf("Error\nInvalid RGB format: %s\n", line), 1);
     }
-    r = ft_atoi(split[0]);
-    g = ft_atoi(split[1]);
-    b = ft_atoi(split[2]);
+
+    // Trim pour éviter les espaces ex: "220, 100, 20"
+    r_str = ft_strtrim(split[0], " \t");
+    g_str = ft_strtrim(split[1], " \t");
+    b_str = ft_strtrim(split[2], " \t");
+
+    // Trim malloc failed ?
+    if (!r_str || !g_str || !b_str)
+    {
+        free(r_str);
+        free(g_str);
+        free(b_str);
+        ft_free_tab(split);
+        return (printf("Error\nMalloc failed\n"), 1);
+    }
+
+    // Convertir
+    r = ft_atoi(r_str);
+    g = ft_atoi(g_str);
+    b = ft_atoi(b_str);
+
+    free(r_str);
+    free(g_str);
+    free(b_str);
+    ft_free_tab(split);
 
     if (!is_valid_rgb(r, g, b))
-        return (1);
+        return (printf("Error\nRGB must be between 0 and 255\n"), 1);
+
     color->red = r;
     color->green = g;
     color->blue = b;
     color->value = convert_rgb_to_int(r, g, b);
+
     return (0);
 }
